9f70b6562dd440fdb031a57c7f6178281e506c1d
VinnyHardth <vinicius.fonseca@icomp.ufam.edu.br>
2024-09-02 14:46:33 -0400

    💾 [PRISMA] Atualizando migracao e esquema prisma
    Descrição detalhada:
    - Refeita a migracao inicial
    - Adicionado tipos a tabela campaign
    - Agora stat name é unico

--- backend/prisma/migrations/20240902184540_initial_migration/migration.sql
+++ backend/prisma/migrations/20240902184540_initial_migration/migration.sql
@@ -16,20 +16,32 @@ CREATE TABLE `User` (
 -- CreateTable
 CREATE TABLE `System` (
     `id` VARCHAR(191) NOT NULL,
-    `name` VARCHAR(191) NOT NULL,
-    `version` VARCHAR(191) NOT NULL,
+    `name` VARCHAR(50) NOT NULL,
+    `version` VARCHAR(10) NOT NULL,
     `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
     `updatedAt` DATETIME(3) NOT NULL,
-    `deletedAt` DATETIME(3) NULL,
 
+    UNIQUE INDEX `System_name_key`(`name`),
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 -- CreateTable
 CREATE TABLE `Stat` (
     `id` VARCHAR(191) NOT NULL,
-    `name` VARCHAR(191) NOT NULL,
-    `description` VARCHAR(191) NOT NULL,
+    `name` VARCHAR(50) NOT NULL,
+    `description` TEXT NOT NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+
+    UNIQUE INDEX `Stat_name_key`(`name`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `SystemStat` (
+    `id` VARCHAR(191) NOT NULL,
+    `system_id` VARCHAR(191) NOT NULL,
+    `stat_id` VARCHAR(191) NOT NULL,
     `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
     `updatedAt` DATETIME(3) NOT NULL,
     `deletedAt` DATETIME(3) NULL,
@@ -42,7 +54,8 @@ CREATE TABLE `Campaign` (
     `id` VARCHAR(191) NOT NULL,
     `user_id` VARCHAR(191) NOT NULL,
     `system_id` VARCHAR(191) NOT NULL,
-    `name` VARCHAR(191) NOT NULL,
+    `name` VARCHAR(100) NOT NULL,
+    `description` TEXT NOT NULL,
     `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
     `updatedAt` DATETIME(3) NOT NULL,
     `deletedAt` DATETIME(3) NULL,
@@ -102,6 +115,12 @@ CREATE TABLE `CharacterSecondaryStat` (
 
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- AddForeignKey
+ALTER TABLE `SystemStat` ADD CONSTRAINT `SystemStat_system_id_fkey` FOREIGN KEY (`system_id`) REFERENCES `System`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `SystemStat` ADD CONSTRAINT `SystemStat_stat_id_fkey` FOREIGN KEY (`stat_id`) REFERENCES `Stat`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE `Campaign` ADD CONSTRAINT `Campaign_user_id_fkey` FOREIGN KEY (`user_id`) REFERENCES `User`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
